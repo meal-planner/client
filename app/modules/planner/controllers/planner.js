a34ca0c9941aec63acd33c462e444636163a21d3
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2015-06-28 00:32:23 +0200

    chore: wrapped controllers in IIFE.

--- app/modules/planner/controllers/planner.js
+++ app/modules/planner/controllers/planner.js
@@ -1,57 +1,59 @@
-'use strict';
-
-/**
- * @ngdoc function
- * @name mealPlanner.planner.controller:PlannerController
- * @description
- * # PlannerController
- * Planner controller
- */
-angular.module('mealPlanner.planner')
-  .controller('PlannerController', PlannerController);
-
-/* @ngInject */
-function PlannerController($scope, planService) {
-  var self = this;
-
-  self.plan = planService.days;
-  self.schedule = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday'
-  ];
-  self.planNextDay = planNextDay;
+(function () {
+  'use strict';
 
   /**
-   * Persist plan if it's updated.
+   * @ngdoc function
+   * @name mealPlanner.planner.controller:PlannerController
+   * @description
+   * # PlannerController
+   * Planner controller
    */
-  $scope.$watch(function () {
-    return planService.days
-  }, function () {
-    planService.persist()
-  }, true);
+  angular.module('mealPlanner.planner')
+    .controller('PlannerController', PlannerController);
 
-  /**
-   * Set initial state.
-   */
-  return initialize();
+  /* @ngInject */
+  function PlannerController($scope, planService) {
+    var self = this;
 
-  function initialize() {
-    if (self.plan.length > 0) {
-      self.schedule.splice(0, self.plan.length);
-    } else {
-      planNextDay();
+    self.plan = planService.days;
+    self.schedule = [
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday'
+    ];
+    self.planNextDay = planNextDay;
+
+    /**
+     * Persist plan if it's updated.
+     */
+    $scope.$watch(function () {
+      return planService.days
+    }, function () {
+      planService.persist()
+    }, true);
+
+    /**
+     * Set initial state.
+     */
+    return initialize();
+
+    function initialize() {
+      if (self.plan.length > 0) {
+        self.schedule.splice(0, self.plan.length);
+      } else {
+        planNextDay();
+      }
+    }
+
+    /**
+     * Add new day to the plan.
+     */
+    function planNextDay() {
+      planService.addDayToPlan(self.schedule.shift());
     }
   }
-
-  /**
-   * Add new day to the plan.
-   */
-  function planNextDay() {
-    planService.addDayToPlan(self.schedule.shift());
-  }
-}
+})();
--- app/modules/planner/controllers/recipe_selector.js
+++ app/modules/planner/controllers/recipe_selector.js
@@ -1,77 +1,79 @@
-'use strict';
-
-/**
- * @ngdoc function
- * @name mealPlanner.planner.controller:RecipeSelectorController
- * @description
- * # RecipeSelectorController
- * Recipe selector controller
- */
-angular.module('mealPlanner.planner')
-  .controller('RecipeSelectorController', RecipeSelectorController);
-
-/* @ngInject */
-function RecipeSelectorController($scope, $mdDialog, $mdUtil, recipeService, planService) {
-  var RECIPE_SEARCH_LIMIT = 4;
-  var self = this;
-
-  self.searchText = null;
-  self.recipes = [];
-  self.searchRecipes = searchRecipes;
-  self.cancelDialog = cancelDialog;
-  self.addRecipe = addRecipe;
+(function () {
+  'use strict';
 
   /**
-   * Set initial state.
+   * @ngdoc function
+   * @name mealPlanner.planner.controller:RecipeSelectorController
+   * @description
+   * # RecipeSelectorController
+   * Recipe selector controller
    */
-  return initialize();
+  angular.module('mealPlanner.planner')
+    .controller('RecipeSelectorController', RecipeSelectorController);
 
-  function initialize() {
-    $scope.$watch('ctrl.searchText', $mdUtil.debounce(searchRecipes, 300));
-    self.mealTypes = planService.mealTypes;
-    self.mealType = planService.mealTypes[0];
+  /* @ngInject */
+  function RecipeSelectorController($scope, $mdDialog, $mdUtil, recipeService, planService) {
+    var RECIPE_SEARCH_LIMIT = 4;
+    var self = this;
 
-    recipeService.getRecipes()
-      .then(function (data) {
-        self.recipes = data;
-        return self.recipes;
-      });
+    self.searchText = null;
+    self.recipes = [];
+    self.searchRecipes = searchRecipes;
+    self.cancelDialog = cancelDialog;
+    self.addRecipe = addRecipe;
+
+    /**
+     * Set initial state.
+     */
+    return initialize();
+
+    function initialize() {
+      $scope.$watch('ctrl.searchText', $mdUtil.debounce(searchRecipes, 300));
+      self.mealTypes = planService.mealTypes;
+      self.mealType = planService.mealTypes[0];
+
+      recipeService.getRecipes()
+        .then(function (data) {
+          self.recipes = data;
+          return self.recipes;
+        });
+    }
+
+    /**
+     * Search recipes with given query.
+     *
+     * @param searchQuery
+     * @param previousSearchText
+     * @returns {*}
+     */
+    function searchRecipes(searchQuery, previousSearchText) {
+      if (!searchQuery && searchQuery === previousSearchText) return;
+      self.isLoading = true;
+
+      return recipeService.searchRecipes(self.searchText, RECIPE_SEARCH_LIMIT)
+        .then(function (data) {
+          self.isLoading = false;
+          self.recipes = data;
+          return self.recipes;
+        }
+      );
+    }
+
+    /**
+     * Send selected recipe to day controller.
+     *
+     * @param recipe
+     */
+    function addRecipe(recipe) {
+      recipe.mealType = self.mealType;
+      $mdDialog.hide(recipe);
+    }
+
+    /**
+     * Close popup.
+     */
+    function cancelDialog() {
+      $mdDialog.cancel();
+    }
   }
-
-  /**
-   * Search recipes with given query.
-   *
-   * @param searchQuery
-   * @param previousSearchText
-   * @returns {*}
-   */
-  function searchRecipes(searchQuery, previousSearchText) {
-    if (!searchQuery && searchQuery === previousSearchText) return;
-    self.isLoading = true;
-
-    return recipeService.searchRecipes(self.searchText, RECIPE_SEARCH_LIMIT)
-      .then(function (data) {
-        self.isLoading = false;
-        self.recipes = data;
-        return self.recipes;
-      }
-    );
-  }
-
-  /**
-   * Send selected recipe to day controller.
-   *
-   * @param recipe
-   */
-  function addRecipe(recipe) {
-    recipe.mealType = self.mealType;
-    $mdDialog.hide(recipe);
-  }
-
-  /**
-   * Close popup.
-   */
-  function cancelDialog() {
-    $mdDialog.cancel();
-  }
-}
+})();
