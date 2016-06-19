d2f720571fe184c95d9dd26771088240ccc32cfa
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2016-06-19 16:17:16 +0300

    ingredients: add search to ingredients group page

--- app/modules/ingredients/controllers/groups.js
+++ app/modules/ingredients/controllers/groups.js
@@ -14,9 +14,30 @@
     .controller('IngredientsGroupsController', IngredientsGroupsController);
 
   /* @ngInject */
-  function IngredientsGroupsController(groups) {
+  function IngredientsGroupsController(NavigationService, IngredientService, groups) {
     var self = this;
 
     self.groups = groups;
+    self.searchIngredients = searchIngredients;
+    NavigationService.navigationBar.searchCallback = searchIngredients;
+
+    /**
+     * Search ingredients by given text query.
+     *
+     * @param {String} query
+     * @returns {*}
+     */
+    function searchIngredients(query) {
+      if (query.length > 0) {
+        NavigationService.navigationBar.isLoading = true;
+        IngredientService.searchIngredients(query)
+          .then(function (data) {
+            NavigationService.navigationBar.isLoading = false;
+            self.items = data;
+          });
+      } else {
+        self.items = null;
+      }
+    }
   }
 })();
